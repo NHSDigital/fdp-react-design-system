0290c408e36ff55e94bab686af0b73a7817d65e2
Fergus Bisset <hello@fergusbisset.com>
2025-08-01 18:19:44 +0100

    Updated storybook story location

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,9 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    '../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
   "addons": [
     "@chromatic-com/storybook",

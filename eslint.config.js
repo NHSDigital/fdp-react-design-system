6617317c7e508ad0b46474b762c25ea53f4d4482
Fergus Bisset <hello@fergusbisset.com>
2025-08-16 09:39:57 +0100

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,30 @@
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import storybook from 'eslint-plugin-storybook';
+<<<<<<< HEAD
 
 export default [
   {
     ignores: ['dist/**', 'node_modules/**']
   },
+=======
+import { FlatCompat } from '@eslint/eslintrc';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default [
+  { ignores: ['dist/**', 'node_modules/**'] },
+  // Bring in the legacy shareable config via compat
+  ...compat.extends('plugin:storybook/recommended'),
+>>>>>>> 21456ba (Update eslint config)
   {
     files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       parser: tsParser,
+<<<<<<< HEAD
       parserOptions: {
         ecmaVersion: 2022,
         sourceType: 'module'
@@ -24,5 +39,11 @@ export default [
     ],
     rules: {
     }
+=======
+      parserOptions: { ecmaVersion: 2022, sourceType: 'module' }
+    },
+    plugins: { '@typescript-eslint': tsPlugin, storybook },
+    rules: {}
+>>>>>>> 21456ba (Update eslint config)
   }
 ];

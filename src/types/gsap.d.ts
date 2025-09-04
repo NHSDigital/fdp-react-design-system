03b8545b2fd21077939cb74624aace80f746a73e
Fergus Bisset <hello@fergusbisset.com>
2025-09-04 18:16:35 +0100

    Removed types stub

--- src/types/gsap.d.ts
+++
@@ -1,8 +0,0 @@
-// Minimal stub for optional 'gsap' import to satisfy type checking when dependency not installed.
-declare module 'gsap' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export const gsap: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const _default: any;
-  export default _default;
-}
